83a3540038e0ef967a79cf8272e71a2c85ecf1b9
brad ferrell <ferrellbh@gmail.com>
2018-04-03 17:58:46 -0400

    adding db schema and seed data

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Nasty", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Schloppy Papi", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Yum Yum", False);
