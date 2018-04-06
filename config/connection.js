185250adef964355f2af0e8afa7d615e7060d7b3
brad ferrell <ferrellbh@gmail.com>
2018-04-05 20:32:08 -0400

    updated connection.js to include JAWSDB for heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 //mysql dependency
 var mysql = require("mysql");
+var connection;
 
-//establishing local connection to db
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  })
+}
 
 connection.connect(function (err) {
   if (err) {
